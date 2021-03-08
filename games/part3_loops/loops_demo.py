9e818fcd4f66cac2f5fa36d3bead66660a492616
artemb <a.bobrovskij@gmail.com>
2021-03-08 16:59:28 +0000

    Loops

---
+++ games/part3_loops/loops_demo.py
@@ -0,0 +1,18 @@
+# Repetitive tasks
+
+# Loop / Iteration
+
+# for loop <-- when you know how many iterations you need
+
+# loop body
+
+# using variables in range
+
+# iteration counter
+
+# while loops
+
+# infinite loops
+
+# break statemen
+
